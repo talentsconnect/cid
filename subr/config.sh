90e1b87e38339397445cd1856ffd2c6250c864df
Michael Grünewald <michipili@gmail.com>
2018-10-06 09:40:03 +0200

    Insert licence blob

--- subr/config.sh
+++ subr/config.sh
@@ -1,5 +1,16 @@
 ### config.sh -- Read Application configuration
 
+# El Cid (https://github.com/michipili/cid)
+# This file is part of El Cid
+#
+# Copyright © 2018 Michael Grünewald
+#
+# This file must be used under the terms of the MIT license.
+# This source file is licensed as described in the file LICENSE, which
+# you should have received as part of this distribution. The terms
+# are also available at
+# https://opensource.org/licenses/MIT
+
 : ${config_file:=/dev/null}
 
 # config CONFIGURATION-KEY
--- subr/stdlib.sh
+++ subr/stdlib.sh
@@ -1,5 +1,17 @@
 ### stdlib.sh -- A standard library for shell programming
 
+# El Cid (https://github.com/michipili/cid)
+# This file is part of El Cid
+#
+# Copyright © 2018 Michael Grünewald
+#
+# This file must be used under the terms of the MIT license.
+# This source file is licensed as described in the file LICENSE, which
+# you should have received as part of this distribution. The terms
+# are also available at
+# https://opensource.org/licenses/MIT
+
+
 #
 # Logging and Interaction
 #
