50b9ab745aba5782f9a6264ed895b0837ff3c62b
Michael Grünewald <michipili@gmail.com>
2018-11-09 07:24:39 +0100

    fixup! Implement basic role support

--- subr/role.sh
+++ subr/role.sh
@@ -26,26 +26,84 @@
 
 role_user_db()
 {
-    cat <<EOF
-root|Charlie Root|admin
-EOF
+    local userdir userconf username displayname role
+
+    if ! [ $# -eq 1 ]; then
+        failwith -x 70 'role_user_db: Invalid call.'
+    fi
+
+    {
+        if [ -d "${config_dir}/user" ]; then
+            find "${config_dir}/user" -type d -maxdepth 1 -mindepth 1
+        fi
+    } | {
+        while read userdir; do
+            username="${userdir##*/}"
+            displayname=$(role_user_config "${username}" 'displayname')
+            role=$(role_user_config "${username}" 'role')
+            if ( role_user_config "${username}" 'environment' | grep -q "$1\|[*]" ); then
+                printf '%s|%s|%s\n' "${username}" "${displayname}" "${role}"
+            fi
+        done
+    }
 }
 
+
+# role_user_config USER CONFIGURATION-KEY
+#  Get and set configuration values
+#
+# The configuration values are read from the corresponding user.conf
+# file or determined from the hardwired strategy when not set.
+
+role_user_config()
+{
+    local userconf value
+    if [ -f "${config_dir}/$1/user.conf" ]; then
+        userconf="${config_dir}/$1/user.conf"
+    else
+        userconf='/dev/null'
+    fi
+    value=$(git config -f "${userconf}" "default.$2")
+    if [ -z "${value}" ]; then
+        case "$2" in
+            displayname)
+                value="$1"
+                ;;
+            role)
+                value='developer'
+                ;;
+            environment)
+                value='*'
+                ;;
+        esac
+    fi
+    printf '%s' "${value}"
+}
 
 # role_user_secret USERNAME
 #  Print the secret associated to USERNAME
 
 role_user_secret()
 {
-    case "$1" in
-        root)
-            printf 'CaitVuelpUrreyftUryoyWishyhuft'
-            ;;
-        *)
-            wlog 'Error' '%s: User does not exist.' "$1"
-            exit 70
-            ;;
-    esac
+    if [ -f "${config_dir}/user/$1/secret" ]; then
+        cat "${config_dir}/user/$1/secret"
+    else
+        failwith 'role_user_secret: %s: User has no secret.' "$1"
+    fi
+}
+
+
+# role_user_authorized_keys USERNAME
+#  Print the authorized keys associated to USERNAME
+
+role_user_authorized_keys()
+{
+    if [ -f "${config_dir}/user/$1/authorized_keys" ]; then
+        # Using sed ensures the file ends with a newline character.
+        sed -e '' "${config_dir}/user/$1/authorized_keys"
+    else
+        failwith 'role_user_authorized_keys: %s: User has no authorized keys.' "$1"
+    fi
 }
 
 ### End of file `role.sh'
